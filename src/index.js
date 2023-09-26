f47efbd4e8a02eda7ad275a92a228119d95ee302
Vitor Ceolin <39313422+vceolin@users.noreply.github.com>
2023-09-25 22:05:14 -0300

    fix: get front end form submitted

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/protected-route", authMiddleware, mainRoutes);
 app.use("/", frontendRoutes);
